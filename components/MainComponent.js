97a93b689e9ea73eafb090d58700b095c9c57050
coderylicode <rylidavis94@gmail.com>
2021-11-23 23:28:25 -0800

    Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -26,6 +26,7 @@ import {
 	fetchPartners,
 } from "../redux/ActionCreators";
 import Reservation from "./ReservationComponent";
+import Favorites from "./FavoritesComponent";
 
 const mapDispatchToProps = {
 	fetchCampsites,
@@ -165,6 +166,31 @@ const ReservationNavigator = createStackNavigator(
 	}
 );
 
+const FavoritesNavigator = createStackNavigator(
+	{
+		Favorites: { screen: Favorites },
+	},
+	{
+		defaultNavigationOptions: ({ navigation }) => ({
+			headerStyle: {
+				backgroundColor: "#5637DD",
+			},
+			headerTintColor: "#fff",
+			headerTitleStyle: {
+				color: "#fff",
+			},
+			headerLeft: (
+				<Icon
+					name="heart"
+					type="font-awesome"
+					iconStyle={styles.stackIcon}
+					onPress={() => navigation.toggleDrawer()}
+				/>
+			),
+		}),
+	}
+);
+
 const CustomDrawerContentComponent = (props) => (
 	<ScrollView>
 		<SafeAreaView
@@ -214,6 +240,16 @@ const MainNavigator = createDrawerNavigator(
 				),
 			},
 		},
+		Favorites: {
+			screen: FavoritesNavigator,
+			navigationOptions: {
+				drawerLabel: "My Favorites",
+				drawerIcon: ({ tintColor }) => (
+					<Icon name="heart" type="font-awesome" size={24} color={tintColor} />
+				),
+			},
+		},
+
 		About: {
 			screen: AboutNavigator,
 			navigationOptions: {
